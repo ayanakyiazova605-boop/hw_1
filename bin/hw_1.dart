ef20636188a5e5459e70784dc3c888265979354d
Аяна <ayana@MacBook-Air-Aana.local>
2025-12-27 18:07:17 +0600

    -

--- bin/hw_1.dart
+++ bin/hw_1.dart
@@ -10,4 +10,54 @@ void main() {
   print('i live $city.');
   print('My profession is $profession');
   print('In my free time, I enjoy $hobby.');
+
+  int salary = 50000;
+  int mounth = 12;
+  int procent = 10;
+
+  int income = salary * mounth;
+  int bonus = income + (income * procent) ~/ 100;
+
+  print('My yeraly income: $income USD');
+  print('My yeraly income with 10%bonus: $bonus USD');
+
+  String tekst =' Knowledge is power,but practice makes perfect. ';
+  print(tekst);
+
+  tekst = tekst.trim();
+  print(tekst);
+
+  tekst = tekst.toUpperCase();
+  print(tekst);
+
+  tekst = tekst.replaceAll('PRACTICE', 'EXPERIENCE');
+  print(tekst);
+
+  tekst = tekst.contains('POWER') ? 'YES' : 'NO';
+  print(tekst);
+
+  int apples = 15;
+  int people = 4;
+
+  int applesPerPerson = apples ~/ people;
+  print('Each person gets $applesPerPerson apples.');
+
+  int left = apples % people;
+  print('Apples left: $left');
+
+  int currentYear = 2025;
+  int myAge = 18;
+  print(currentYear - myAge);
+
+  var city1 = 'Bishkek';
+  final country = 'Kyrgyzstan';
+  const planet = 'Earth';
+
+  city1 = 'Osh';
+  country = 'Kazakhstan';
+  planet = 'Mars';
+
+  print(city1);
+  print(country);
+  print(planet);
 }
