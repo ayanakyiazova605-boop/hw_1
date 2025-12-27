e1be54276de71c8a02b90804e645458237b5bf06
Аяна <ayana@MacBook-Air-Aana.local>
2025-12-27 17:26:24 +0600

    first commit

---
+++ bin/hw_1.dart
@@ -0,0 +1,13 @@
+void main() {
+  String name = 'Alice';
+  int age = 30;
+  String city = 'New York';
+  String profession = 'Teacher';
+  String hobby = 'football';
+
+  print('Hello, my name is $name');
+  print('I am $age years old.');
+  print('i live $city.');
+  print('My profession is $profession');
+  print('In my free time, I enjoy $hobby.');
+}
